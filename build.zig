ceab34aa9eba3b841fdf6ca94fa2e0fbc56f6743
karol1604 <karol.gromada16@gmail.com>
2025-08-09 04:33:52 +0200

    turned the thing into an actual library

--- build.zig
+++ build.zig
@@ -26,19 +26,39 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    const lib_mod = b.createModule(.{
+        // `root_source_file` is the Zig "entry point" of the module. If a module
+        // only contains e.g. external object files, you can make this `null`.
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe_mod.addImport("metalzig", lib_mod);
+
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "metalzig",
+        .root_module = lib_mod,
+    });
+
+    lib.linkLibC();
+    lib.linkFramework("Metal");
+    lib.linkFramework("Foundation");
+    lib.linkFramework("MetalKit");
+    lib.addIncludePath(b.path("c-shim"));
+    lib.addObjectFile(b.path("build-artifacts/libmetalshim.a"));
+
+    b.installArtifact(lib);
+
     // This creates another `std.Build.Step.Compile`, but this one builds an executable
     // rather than a static library.
     const exe = b.addExecutable(.{
         .name = "metal_zig",
         .root_module = exe_mod,
     });
-
-    exe.linkLibC();
-    exe.linkFramework("Metal");
-    exe.linkFramework("Foundation");
-    exe.linkFramework("MetalKit");
-    exe.addIncludePath(b.path("c-shim"));
-    exe.addObjectFile(b.path("build-artifacts/libmetalshim.a"));
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
@@ -68,6 +88,12 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
+    const lib_unit_tests = b.addTest(.{
+        .root_module = lib_mod,
+    });
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
     const exe_unit_tests = b.addTest(.{
         .root_module = exe_mod,
     });
@@ -79,4 +105,5 @@ pub fn build(b: *std.Build) void {
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_lib_unit_tests.step);
 }
---
+++ src/root.zig
@@ -0,0 +1,173 @@
+const std = @import("std");
+const c = @cImport({
+    @cInclude("metal_shim.h");
+});
+
+pub const MTLSize = struct {
+    width: usize,
+    height: usize,
+    depth: usize,
+
+    pub fn init(width: usize, height: usize, depth: usize) MTLSize {
+        return MTLSize{ .width = width, .height = height, .depth = depth };
+    }
+};
+
+pub const ComputeCommandEncoder = struct {
+    handle: *c.MTLComputeCommandEncoderHandle,
+    pub fn deinit(self: *ComputeCommandEncoder) void {
+        c.mtl_release_compute_command_encoder(self.handle);
+    }
+
+    pub fn setComputePipelineState(self: *ComputeCommandEncoder, pipeline: *ComputePipelineState) void {
+        c.mtl_enc_set_compute_pipeline_state(self.handle, pipeline.handle);
+    }
+
+    pub fn setBuffer(self: *ComputeCommandEncoder, buffer: *Buffer, offset: usize, index: u32) void {
+        c.mtl_enc_set_buffer(self.handle, buffer.handle, @intCast(offset), @intCast(index));
+    }
+
+    pub fn setBytes(self: *ComputeCommandEncoder, bytes: []const u8, index: u32) void {
+        c.mtl_enc_set_bytes(self.handle, @intCast(index), @intCast(bytes.len), @ptrCast(bytes.ptr));
+    }
+
+    pub fn dispatchThreads(self: *ComputeCommandEncoder, threadsPerGridX: usize, threadsPerGridY: usize, threadsPerGridZ: usize, threadsPerThreadgroupX: usize, threadsPerThreadgroupY: usize, threadsPerThreadgroupZ: usize) void {
+        c.mtl_enc_dispatch_threads(
+            self.handle,
+            @intCast(threadsPerGridX),
+            @intCast(threadsPerGridY),
+            @intCast(threadsPerGridZ),
+            @intCast(threadsPerThreadgroupX),
+            @intCast(threadsPerThreadgroupY),
+            @intCast(threadsPerThreadgroupZ),
+        );
+    }
+
+    pub fn endEncoding(self: *ComputeCommandEncoder) void {
+        c.mtl_end_encoding(self.handle);
+    }
+};
+
+pub const CommandBuffer = struct {
+    handle: *c.MTLCommandBufferHandle,
+    pub fn deinit(self: *CommandBuffer) void {
+        c.mtl_release_command_buffer(self.handle);
+    }
+
+    pub fn newComputeCommandEncoder(self: *CommandBuffer) !ComputeCommandEncoder {
+        const encoder = c.mtl_new_compute_command_encoder(self.handle);
+        if (encoder == null) return error.ComputeCommandEncoderCreationFailed;
+        return ComputeCommandEncoder{ .handle = encoder.? };
+    }
+
+    pub fn commit(self: *CommandBuffer) void {
+        c.mtl_command_buffer_commit(self.handle);
+    }
+
+    pub fn waitUntilCompleted(self: *CommandBuffer) void {
+        c.mtl_command_buffer_wait_until_completed(self.handle);
+    }
+};
+
+pub const CommandQueue = struct {
+    handle: *c.MTLCommandQueueHandle,
+    pub fn deinit(self: *CommandQueue) void {
+        c.mtl_release_command_queue(self.handle);
+    }
+
+    pub fn newCommandBuffer(self: *CommandQueue) !CommandBuffer {
+        const command_buffer = c.mtl_new_command_buffer(self.handle);
+        if (command_buffer == null) return error.CommandBufferCreationFailed;
+        return CommandBuffer{ .handle = command_buffer.? };
+    }
+};
+
+pub const Function = struct {
+    handle: *c.MTLFunctionHandle,
+    pub fn deinit(self: *Function) void {
+        c.mtl_release_function(self.handle);
+    }
+};
+
+pub const Library = struct {
+    handle: *c.MTLLibraryHandle,
+    pub fn deinit(self: *Library) void {
+        c.mtl_release_library(self.handle);
+    }
+
+    pub fn newFunctionWithName(self: *Library, name: [*c]const u8) !Function {
+        const function = c.mtl_new_function_with_name(self.handle, name);
+        if (function == null) return error.FunctionCreationFailed;
+        return Function{ .handle = function.? };
+    }
+};
+
+pub const ComputePipelineState = struct {
+    handle: *c.MTLComputePipelineStateHandle,
+    pub fn deinit(self: *ComputePipelineState) void {
+        c.mtl_release_compute_pipeline_state(self.handle);
+    }
+
+    pub fn getMaxTotalThreadsPerThreadgroup(self: *ComputePipelineState) usize {
+        return c.mtl_get_maxTotalThreadsPerThreadgroup(self.handle);
+    }
+};
+
+pub const Buffer = struct {
+    handle: *c.MTLBufferHandle,
+    len: usize,
+
+    pub fn deinit(self: *Buffer) void {
+        c.mtl_release_buffer(self.handle);
+    }
+
+    /// Returns the buffer contents as a slice of bytes.
+    pub fn getContents(self: *Buffer) ?[]u8 {
+        const contents: [*]u8 = @ptrCast(c.mtl_buffer_get_contents(self.handle));
+        // if (contents == null) return null;
+        return contents[0..self.len];
+    }
+
+    /// Returns the buffer contents as a slice of type `T`.
+    pub fn getContentsAs(self: *Buffer, comptime T: type) ?[]T {
+        const contents = self.getContents() orelse return null;
+        return @alignCast(std.mem.bytesAsSlice(T, contents));
+    }
+};
+
+pub const Device = struct {
+    handle: *c.MTLDeviceHandle,
+    pub fn init() !Device {
+        const device = c.mtl_create_system_default_device();
+        if (device == null) return error.NoDevice;
+        return Device{ .handle = device.? };
+    }
+
+    pub fn deinit(self: *Device) void {
+        c.mtl_release_device(self.handle);
+    }
+
+    pub fn newCommandQueue(self: *Device) !CommandQueue {
+        const queue = c.mtl_new_command_queue(self.handle);
+        if (queue == null) return error.CommandQueueCreationFailed;
+        return CommandQueue{ .handle = queue.? };
+    }
+
+    pub fn newLibraryWithURL(self: *Device, url: [*c]const u8) !Library {
+        const library = c.mtl_new_library_with_url(self.handle, url);
+        if (library == null) return error.LibraryCreationFailed;
+        return Library{ .handle = library.? };
+    }
+
+    pub fn newComputePipelineStateWithFunction(self: *Device, function: *Function) !ComputePipelineState {
+        const pipeline = c.mtl_new_compute_pipeline_state_with_function(self.handle, function.handle);
+        if (pipeline == null) return error.PipelineCreationFailed;
+        return ComputePipelineState{ .handle = pipeline.? };
+    }
+
+    pub fn newBufferWithLength(self: *Device, length: usize, options: c.MTLResourceOptionsHandle) !Buffer {
+        const buffer = c.mtl_new_buffer_with_length(self.handle, @intCast(length), options);
+        if (buffer == null) return error.BufferCreationFailed;
+        return Buffer{ .handle = buffer.?, .len = length };
+    }
+};
